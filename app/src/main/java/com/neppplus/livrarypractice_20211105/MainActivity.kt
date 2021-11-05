3fdd3de3c98f6f37235092cbad3cc63217fd700a
ujin-Bang <qkddnwls14@gmail.com>
2021-11-05 17:12:36 +0900

    실제 권한 물어보는 코드 작성

--- app/src/main/java/com/neppplus/livrarypractice_20211105/MainActivity.kt
+++ app/src/main/java/com/neppplus/livrarypractice_20211105/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.neppplus.livrarypractice_20211105
 
+import android.Manifest
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +20,38 @@ class MainActivity : AppCompatActivity() {
 
 
     fun setupEvents(){
+
+        btnCall.setOnClickListener() {
+//        권한 획득
+//            1)권한 여하에 /따른 사황별 대처방안 미리 변수에 담아두자
+
+            val permissionListener = object : PermissionListener {
+                override fun onPermissionGranted() {
+
+//                    권한이 최종 획등 ok 일때 실행할 코드
+
+                    val myUri = Uri.parse("tel:010-5555-6666")
+                    val myIntent = Intent(Intent.ACTION_CALL,myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+//                    최종 권한 거부 되었을 때
+                    Toast.makeText(this@MainActivity,"권한이 거부되어 전화안됨",Toast.LENGTH_SHORT).show()
+
+                }
+
+
+            }
+
+//            게획 수립은 완료. 실제로 권한이 있는지 물어보자.
+            TedPermission.create()
+                .setPermissionListener(permissionListener)
+                .setPermissions( Manifest.permission.CALL_PHONE )
+                .check()
+
+        }
 
 //        이미지뷰 / 텍스트뷰 / LinearLayout 등은 버튼처럼
         imgProfile.setOnClickListener() {
